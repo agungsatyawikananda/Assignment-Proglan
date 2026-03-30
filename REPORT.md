4688e1547de796de3ae4e7d68329ba78b80e8bf6
agungsatyawikananda <agungsatyawikananda@gmail.com>
2026-03-30 15:55:29 +0700

    Assignment 1 Selesai

---
+++ REPORT.md
@@ -0,0 +1,5 @@
+# Buat Laporan di Sini
+
+Mengubah program untuk menerima input dan mengeluarkan output yang sesuai
+
+Mengubah nama file output dari 'myapp' ke 'MyApp'
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+    cout << "Silakan inputkan nama anda:  ";
+    string nama;
+    cin >> nama;
+    cout << "Halo " << nama << endl;
+
+    return 0;
+}
