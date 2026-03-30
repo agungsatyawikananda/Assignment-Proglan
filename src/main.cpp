1a383984374dbefbc4f6ff044f5ccf22427adfe4
agungsatyawikananda <agungsatyawikananda@gmail.com>
2026-03-30 16:10:19 +0700

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(int argc, char ** argv) {
-    cout << "Silakan inputkan nama anda:  ";
+    cout << "Silakan inputkan nama anda: ";
     string nama;
     cin >> nama;
     cout << "Halo " << nama << endl;
